a143ebd31148b788d621db9b8fd4f1dd4ea42638
usmantasaddaq <usmantasaddaq501@gmail.com>
2023-01-17 23:12:16 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import "./App.css";
 import Login from "./Components/LogIn";
 import { Routes, Route } from "react-router-dom";
-import UserAttendance from "./Components/UserAttendance";
-import Admin from './Components/Admin';
-import AuthRoot from "react-auth-simple";
-import AuthRoote from "./Components/AuthRoote";
+
 function App() {
   return (
     <>
       <Routes>
 
         <Route  path="/" element={<Login />} /> 
-       {/* <AuthRoot exact path="/logIn"  element={<AuthRoote/>}/>  */}
-        {/* <Route path="/" element={<Admin />} /> */}
+
       </Routes>
     </>
   );
